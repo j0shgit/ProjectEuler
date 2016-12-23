f12495d83368b48bd8e405ba6237a16d2426b94d
j0shgit <j0sh.oleary11@gmail.com>
2016-12-23 14:58:21 -0600

    Solved Puzzle 2

--- even_fibonacci_numbers.cpp
+++ even_fibonacci_numbers.cpp
@@ -1,3 +1,24 @@
 #include <iostream>
 #include <iostream>
 
+int main() {
+
+
+	long limit = 4000000;
+	long sum;
+	long a = 1;
+	long b = 1;
+	long h;
+
+	while (b < limit) {
+		if (b % 2 == 0) {
+			sum+=b;
+		}
+		h = a+b;
+		a=b;
+		b=h;
+	}
+
+	std::cout << sum << std::endl;
+	return 0;
+}
